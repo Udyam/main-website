a7742fe887362061d8cc194b464aff36d643e4cf
Shubham9455 <shubham.jaiswal.ece21@itbhu.ac.in>
2022-12-23 18:50:28 +0530

    logo click animation

--- src/Landing/Logo/Logo.js
+++ src/Landing/Logo/Logo.js
@@ -1,6 +1,61 @@
 import React from 'react';
 import './Logo.css';
+
+const events = [
+  {
+    id: 1,
+    img: '/assets/eeslogo.svg',
+    title: 'EES'
+  },
+  {
+    id: 2,
+    img: '/assets/udgamlogo.png',
+    title: 'UDGAM'
+  }
+];
+
 const Logo = () => {
+  const [event, setEvent] = React.useState(0);
+  const logoClick = () => {
+    document.getElementsByClassName('main-event-logo')[0].animate(
+      [
+        // keyframes
+        { transform: 'scale(1)' },
+        { transform: 'scale(0)' },
+        { transform: 'scale(1)' }
+      ],
+      {
+        // timing options
+        duration: 1000,
+        iterations: 1
+      }
+    );
+    document.getElementsByClassName('ring-6')[0].animate(
+      [
+        // keyframes
+        { transform: 'rotate(0deg)' },
+        { transform: 'rotate(360deg)' }
+        // { transform: 'scale(1)' }
+      ],
+      {
+        // timing options
+        duration: 1000,
+        iterations: 1
+      }
+    );
+
+    setTimeout(() => {
+      if (event === 0) {
+        setEvent(1);
+      } else {
+        setEvent(0);
+      }
+      // document
+      //   .getElementsByClassName('main-event-logo')[0]
+      //   .classList.toggle('main-event-logo-leaving');
+    }, 500);
+  };
+
   return (
     <div className="main-logo-container">
       <div className="ring-box-1">
@@ -8,12 +63,20 @@ const Logo = () => {
         <img className="rings ring-8" src="/assets/rings/8.png" alt="" />
         <img className="rings ring-7" src="/assets/rings/7.png" alt="" />
         <img className="rings ring-9" src="/assets/rings/9.png" alt="" />
+        <img className="rings ring-6" src="/assets/rings/6.png" alt="" />
         <div className="glass-lens"></div>
         {/* on Hover */}
         <div className="glowing-ring"></div>
         <img className="rings ring-4" src="/assets/rings/4.png" alt="" />
       </div>
-      <img className="main-event-logo" src="/assets/eeslogo.svg" alt="" />
+      <img
+        className="main-event-logo"
+        src={events[event].img}
+        height={'200px'}
+        width={'200px'}
+        alt=""
+        onClick={logoClick}
+      />
       <div className="rays"></div>
     </div>
   );
