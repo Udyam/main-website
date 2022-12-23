87558fa7e5f2168008a197d2d1833005f3cd3cea
mamaleshrh <mamaleshhake@gmail.com>
2022-12-23 22:13:45 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
+import Updates from './components/updates/updates';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Home />
+                <Updates />
+              </>
+            }
+          />
+        </Routes>
+        <Routes>
+          <Route path="/updates" element={<Updates />} />
         </Routes>
       </Router>
     </div>
--- src/components/updates/updates.js
+++ src/components/updates/updates.js
@@ -8,8 +8,12 @@ function Updates() {
         <div className="updates-udyam-content">
           <h1>Lorem ipsum dolor sit.</h1>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate suscipit distinctio
-            facilis voluptate? Reprehenderit esse commodi quo temporibus, ad fugiat.
+            Udyam is the Annual Technological Festival of the department of Electronics Engineering,
+            IIT (BHU) Varanasi. It brings a broad spectrum of events spanning Data Science, Machine
+            Learning, Digital and Analog Electronics and others to boost the participants skill
+            sets. Since 2010, Udyam has provided a platform to develop and showcase the technical
+            skills of Indian technocrats. And we are pleased to announce that the grand event is
+            again returning this spring as UDYAM 23!
           </p>
         </div>
 
@@ -17,8 +21,13 @@ function Updates() {
         <div className="updates-udgam-content">
           <h1>Lorem ipsum dolor sit.</h1>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate suscipit distinctio
-            facilis voluptate? Reprehenderit esse commodi quo temporibus, ad fugiat.
+            Udgam is the Annual Cultural Festival of the department of Electronics Engineering, IIT
+            (BHU) Varanasi. It brings you multifarious events that kindle your creative side and
+            awaken your artistic self. EES introduced Udgam at the start of 2022 to provide a
+            platform for participants to showcase their talents in non-technical fields like fine
+            arts, dance, music, photography, and literature. Udgam is back again, bigger and
+            brighter, and awaiting to witness the vigour its participants would bring to the stage.
+            Get ready to manifest your artistry and let your creative self sparkle!
           </p>
         </div>
 
@@ -26,11 +35,18 @@ function Updates() {
         <div className="updates-mashal-content">
           <h1>Lorem ipsum dolor sit.</h1>
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate suscipit distinctio
-            facilis voluptate? Reprehenderit esse commodi quo temporibus, ad fugiat.
+            Mashal is the sports festival of the department of Electronics Engineering, IIT (BHU)
+            Varanasi.It offers a venue for all significant competitions and sports to foster good
+            sportsmanship among the students.Even though it started last year, the event covers
+            numerous sports, including Marathon, Chess, Valorant, Table Tennis, Basketball, Tennis,
+            Football, Cricket, Badminton, Kabaddi, Athletics, and Tug of War. Mashal unleashes the
+            competitive spirit of the nations brightest minds and offers a perfect arena for sports
+            enthusiasts to face off against one another. As said by Serena Williams,the lets
+            overpower, overtake and overcome!
           </p>
         </div>
       </section>
+      {/* <div className="article"></div> */}
     </>
   );
 }
