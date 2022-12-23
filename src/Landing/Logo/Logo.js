b935f652b4de8057adc0dc9b47bbcc792f861b14
Shubham9455 <shubham.jaiswal.ece21@itbhu.ac.in>
2022-12-23 23:19:52 +0530

    rings done

--- src/Landing/Logo/Logo.js
+++ src/Landing/Logo/Logo.js
@@ -1,36 +1,11 @@
 import React from 'react';
 import './Logo.css';
 import './CircularButton.css';
-const events = [
-  {
-    id: 1,
-    img: '/assets/eeslogo.svg',
-    title: 'EES'
-  },
-  {
-    id: 2,
-    img: '/assets/udgamlogo.png',
-    title: 'UDGAM'
-  }
-];
 
 const Logo = () => {
   const [angle, setAngle] = React.useState(90);
-  const [event, setEvent] = React.useState(0);
   const logoClick = () => {
-    // document.getElementsByClassName('main-event-logo')[0].animate(
-    //   [
-    //     // keyframes
-    //     { transform: 'scale(1)' },
-    //     { transform: 'scale(0)' }
-    //   ],
-    //   {
-    //     // timing options
-    //     duration: 500,
-    //     iterations: 1
-    //   }
-    // );
-    document.getElementsByClassName('ring-6')[0].animate(
+    document.getElementsByClassName('button-container')[0].animate(
       [
         // keyframes
         { transform: 'rotate(' + angle + 'deg)' },
@@ -45,52 +20,49 @@ const Logo = () => {
       }
     );
     setAngle(angle + 120);
-    setTimeout(() => {
-      if (event === 0) {
-        setEvent(1);
-      } else {
-        setEvent(0);
-      }
-      // document
-      //   .getElementsByClassName('main-event-logo')[0]
-      //   .classList.toggle('main-event-logo-leaving');
-    }, 500);
   };
 
   return (
-    <div className="main-logo-container">
-      <div className="ring-box-1">
-        <img height={'400px'} className="rings" src="/assets/New Rings/inner_ring.svg" alt="" />
-        <img
-          className="rings ring-middle"
-          height={'400px'}
-          src="/assets/New Rings/middle_ring.svg"
-          alt=""
-        />
-        <img
-          className="rings dotted-ring"
-          height={'400px'}
-          src="/assets/New Rings/dotted_circle.svg"
-          alt=""
-        />
-        <img className="rings ring-7" height={'400px'} src="/assets/rings/7.png" alt="" />
-        <img className="rings ring-8" height={'400px'} src="/assets/rings/8.png" alt="" />
-        <img className="rings ring-4" height={'400px'} src="/assets/rings/4.png" alt="" />
-        <img className="rings ring-3" height={'400px'} src="/assets/rings/3.png" alt="" />
+    <>
+      <div className="main-container-for-logo">
+        <div className="main-logo-container">
+          <div className="ring-box-1">
+            <img height={'400px'} className="rings" src="/assets/New Rings/inner_ring.svg" alt="" />
+            <img
+              className="rings ring-middle"
+              height={'400px'}
+              src="/assets/New Rings/middle_ring.svg"
+              alt=""
+            />
+            <img
+              className="rings dotted-ring"
+              height={'400px'}
+              src="/assets/New Rings/dotted_circle.svg"
+              alt=""
+            />
+            <img className="rings ring-7" height={'400px'} src="/assets/rings/7.png" alt="" />
+            <img className="rings ring-8" height={'400px'} src="/assets/rings/8.png" alt="" />
+            <img className="rings ring-4" height={'400px'} src="/assets/rings/4.png" alt="" />
+            <img className="rings ring-3" height={'400px'} src="/assets/rings/3.png" alt="" />
 
-        <div className="glass-lens"></div>
-        {/* on Hover */}
-        <div className="glowing-ring"></div>
+            <div className="glass-lens"></div>
+            {/* on Hover */}
+            <div className="glowing-ring"></div>
+          </div>
+          <img
+            className="main-event-logo"
+            src="/assets/eeslogo.svg"
+            height={'200px'}
+            width={'200px'}
+            alt=""
+            onClick={logoClick}
+          />
+        </div>
+        <div className="button-container">
+          <img className="dial-button" src="/assets/rings/final_dial.svg" alt="" />
+        </div>
       </div>
-      <img
-        className="main-event-logo"
-        src={events[event].img}
-        height={'200px'}
-        width={'200px'}
-        alt=""
-        onClick={logoClick}
-      />
-    </div>
+    </>
   );
 };
 
