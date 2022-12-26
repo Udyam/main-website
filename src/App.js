7a8feb949ac18dd06764ee8529b7287b5fe6ae59
Shubham9455 <shubham.jaiswal.ece21@itbhu.ac.in>
2022-12-26 17:18:19 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
-import Updates from './components/updates/updates';
-import Speakers from './components/Speakers/Speakers';
-import Sponsers from './components/Sponsors/sponsors';
-import Footer from './components/footer/footer';
-import About from './components/AboutUS/AboutUs';
-import Faq from './components/faq/Faq';
+
 function App() {
   return (
     <div className="App">
@@ -18,18 +13,9 @@ function App() {
             element={
               <>
                 <Home />
-                <About />
-                <Updates />
-                <Sponsers />
-                <Speakers />
-                <Faq />
-                <Footer />
               </>
             }
           />
-        </Routes>
-        <Routes>
-          <Route path="/updates" element={<Updates />} />
         </Routes>
       </Router>
     </div>
