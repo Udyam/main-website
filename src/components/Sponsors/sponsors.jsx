d5e32b09bb434f123d84ed1c75c23b838a2d74d5
NotAtitYet <kumar.sonu.ece21@itbhu.ac.in>
2023-01-04 01:58:23 +0530

    removed prettier errors

--- src/components/Sponsors/sponsors.jsx
+++ src/components/Sponsors/sponsors.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import SponsorCard from './sponsorCard';
 import './sponsors.css';
 
--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
 import './Faq.css';
 import img from './button.png';
