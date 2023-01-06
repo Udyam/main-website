fc5e2adeeaef3a49c47d6ba3ddcdad91f8b5392e
Rew0611 <harshit.swarnkar.ece21@itbhu.ac.in>
2023-01-06 19:08:41 +0530

    changes

--- src/Udyam/components/about/about.js
+++ src/Udyam/components/about/about.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import React from 'react';
 import './about.css';
 import xd from './images/logo.svg';
@@ -11,8 +10,8 @@ export default function About() {
           <img src={xd} alt="" />
         </div>
         <div className="column udy">
-          <h1 className='hdx'>Udyam</h1>
-          <p className='udx'>
+          <h1 className="hdx">Udyam</h1>
+          <p className="udx">
             UDYAM is the Annual Technical Festival of the Department of Electronics Engineering, IIT (BHU) Varanasi. UDYAM offers an opportunity for students to treat themselves to a technical extravaganza. It gives you a chance to be more actively involved in the learning experience at the heart of effective technology integration. It is a means for students to showcase their practical talent in
             their learning and not just limit themselves to theoretical knowledge. With this idea of an application-oriented approach, UDYAM started in 2010 with a vision of providing a platform for the community of Indian students to develop and showcase their technical prowess. It is not just these head to head competitions that you can get at Udyam. It is the events that make you proficient
             with the industry standards. The limelight is on you.
