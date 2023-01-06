6b6f24270d19910189abcbd8baf75bb789735e85
Rew0611 <harshit.swarnkar.ece21@itbhu.ac.in>
2023-01-06 15:49:07 +0530

    changes

--- src/Udyam/Udyam.js
+++ src/Udyam/Udyam.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Udyam.css';
 import LeaderBoard from './components/LeaderBoard/LeaderBoard';
 import Nav from './components/Nav/Nav';
+// import About from './components/about/about';
 
 const Udyam = () => {
   return (
@@ -9,6 +10,7 @@ const Udyam = () => {
       <div className="udyam-home">
         <Nav />
         <LeaderBoard contentIndex={0} />
+        {/* <About /> */}
       </div>
     </>
   );
