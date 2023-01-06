b2b55bf0765ffe6a81d5a04bf76764648a6cfd1d
Rew0611 <harshit.swarnkar.ece21@itbhu.ac.in>
2023-01-06 17:55:32 +0530

    changes

--- src/Udyam/Udyam.js
+++ src/Udyam/Udyam.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import './Udyam.css';
-import Events from './components/Events/events';
-import LeaderBoard from './components/LeaderBoard/LeaderBoard';
-// import Nav from './components/Nav/Nav';
+// import Events from './components/Events/events';
+// import LeaderBoard from './components/LeaderBoard/LeaderBoard';
+import Nav from './components/Nav/Nav';
+import About from './components/about/about';
 
 const Udyam = () => {
   return (
     <>
-      <div className="udyam-home">
-        {/* <Nav /> */}
-        <Events />
-        <LeaderBoard contentIndex={0} />
-        {/* <About /> */}
+      <div className="udyam-home grid-cols-2">
+        <Nav />
+        {/* <Events /> */}
+        {/* <LeaderBoard contentIndex={0} /> */}
+        <About />
       </div>
     </>
   );
--- src/Udyam/components/about/about.js
+++ src/Udyam/components/about/about.js
@@ -6,12 +6,12 @@ import xd from './images/logo.svg';
 export default function About() {
   return (
     <div className="bg">
-      <div className="row">
+      <div className="aboutx">
         <div className="column imgx">
           <img src={xd} alt="" />
         </div>
         <div className="column udy">
-          <h1>Udyam</h1>
+          <h1 className='hdx'>Udyam</h1>
           <p className='udx'>
             UDYAM is the Annual Technical Festival of the Department of Electronics Engineering, IIT (BHU) Varanasi. UDYAM offers an opportunity for students to treat themselves to a technical extravaganza. It gives you a chance to be more actively involved in the learning experience at the heart of effective technology integration. It is a means for students to showcase their practical talent in
             their learning and not just limit themselves to theoretical knowledge. With this idea of an application-oriented approach, UDYAM started in 2010 with a vision of providing a platform for the community of Indian students to develop and showcase their technical prowess. It is not just these head to head competitions that you can get at Udyam. It is the events that make you proficient
