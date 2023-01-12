e28689e5fe5301abb56c6ebcc3354ca9d75f55e2
avnesh141 <avnesh.kumar.ece21@itbhu.ac.in>
2023-01-12 17:43:57 +0530

    Add links

--- src/Udyam/components/Sponsors/Sponsors.js
+++ src/Udyam/components/Sponsors/Sponsors.js
@@ -8,51 +8,56 @@ const Sponsors = () => {
       <h1>SPONSORS</h1>
       <div className="sponsBox">
         <div className="sponser sponser1">
-          <div className="sp">
-            <h4>Microsoft</h4> <FaChevronCircleRight className="info spicon" />
-          </div>
+          <a href="https://www.wipro.com" className="sp" style={{ textDecoration: 'none' }}>
+            {' '}
+            <h4>Microsoft</h4> <FaChevronCircleRight className="spicon" />
+          </a>
         </div>
         <div className="sponser sponser2">
-          <div className="sp">
+          <a href="https://www.wipro.com" className="sp" style={{ textDecoration: 'none' }}>
+            {' '}
             <h4>Google</h4>
-            <FaChevronCircleRight className="info spicon" />{' '}
-          </div>
+            <FaChevronCircleRight className="spicon" />{' '}
+          </a>
         </div>
         <div className="sponser sponser3">
-          <div className="sp">
+          <a href="https://www.wipro.com" className="sp" style={{ textDecoration: 'none' }}>
+            {' '}
             <h4>CodeChef</h4>
-            <FaChevronCircleRight className="info spicon" />{' '}
-          </div>
+            <FaChevronCircleRight className="spicon" />{' '}
+          </a>
         </div>
         <div className="sponser sponser4">
-          <div className="sp">
+          <a href="https://www.wipro.com" className="sp" style={{ textDecoration: 'none' }}>
+            {' '}
             <h4>Airtel</h4>
-            <FaChevronCircleRight className="info spicon" />{' '}
-          </div>
+            <FaChevronCircleRight className="spicon" />{' '}
+          </a>
         </div>
         <div className="sponser sponser5">
-          <div className="sp">
+          <a href="https://www.wipro.com" className="sp" style={{ textDecoration: 'none' }}>
             <h4>Amazon</h4>
-            <FaChevronCircleRight className="info spicon" />{' '}
-          </div>
+            <FaChevronCircleRight className="spicon" />{' '}
+          </a>
         </div>
         <div className="sponser sponser6">
-          <div className="sp">
+          <a href="https://www.wipro.com" className="sp" style={{ textDecoration: 'none' }}>
             <h4>Capgemini</h4>
-            <FaChevronCircleRight className="info spicon" />
-          </div>
+            <FaChevronCircleRight className="spicon" />
+          </a>
         </div>
         <div className="sponser sponser7">
-          <div className="sp">
+          <a href="https://www.wipro.com" className="sp" style={{ textDecoration: 'none' }}>
             <h4>Wipro</h4>
-            <FaChevronCircleRight className="info spicon" />
-          </div>
+            <FaChevronCircleRight className="spicon" />
+          </a>
         </div>
         <div className="sponser sponser8">
-          <div className="sp">
+          <a href="https://www.wipro.com" className="sp" style={{ textDecoration: 'none' }}>
+            {' '}
             <h4>Flipkart</h4>
-            <FaChevronCircleRight className="info spicon" />
-          </div>
+            <FaChevronCircleRight className="spicon" />
+          </a>
         </div>
       </div>
     </div>
