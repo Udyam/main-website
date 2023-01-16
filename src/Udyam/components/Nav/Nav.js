6bf19ce559f17ce75ef658ba60f7a1a087866f25
mamaleshrh <mamaleshhake@gmail.com>
2023-01-16 19:43:32 +0530

    changes

--- src/Udyam/components/Nav/Nav.js
+++ src/Udyam/components/Nav/Nav.js
@@ -215,16 +215,17 @@ const Nav = (props) => {
               </Link>
             </div>
           </div>
+          <div className="udyam-ees-mobile">
+        <Link to="/">
+          <img className="ees-img-mobile" src={ees} alt="hreo" />
+        </Link>
+      </div>
           {/* <div className="udyam-ees">
                     <img className='ees-img' src={ees} alt="hreo" />
                 </div> */}
         </div>
       )}
-      <div className="udyam-ees-mobile">
-        <Link to="/">
-          <img className="ees-img-mobile" src={ees} alt="hreo" />
-        </Link>
-      </div>
+
       <div className="menu-icon" onClick={menuNav}>
         <TfiMenu />
       </div>
