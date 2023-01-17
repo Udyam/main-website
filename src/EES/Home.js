063ba99529a22b0b62b1defad5429cca402ed666
Nikhil Agarwal <agarwalnikhil576@gmail.com>
2023-01-17 16:01:03 +0530

    updated sponsors and ees home

--- src/EES/Home.js
+++ src/EES/Home.js
@@ -14,19 +14,19 @@ import Sponsors from './Sponsors/sponsors.jsx';
 const eventData = [
   {
     title: 'Udyam',
-    description: 'lorem ipsum',
-    imgUrl: '/assets/udayam.png'
+    description: `Udyam is the Annual Technological Festival of the department of Electronics Engineering, IIT (BHU) Varanasi. It brings a broad spectrum of events spanning Data Science, Machine Learning, Digital and Analog Electronics and others to boost the participant's skill sets. Since 2010, Udyam has provided a platform to develop and showcase the technical skills of Indian technocrats. And we are pleased to announce that the grand event is again returning this spring as UDYAM'23!`,
+    imgUrl: '/assets/eeslogo.svg'
   },
 
   {
     title: 'Mashal',
-    description: 'lorem ipsum',
-    imgUrl: '/assets/udayam.png'
+    description: `Mashal is the sports festival of the department of Electronics Engineering, IIT (BHU) Varanasi.It offers a venue for all significant competitions and sports to foster good sportsmanship among the students.Even though it started last year, the event covers numerous sports, including Marathon, Chess, Valorant, Table Tennis, Basketball, Tennis, Football, Cricket, Badminton, Kabaddi, Athletics, and Tug of War. Mashal unleashes the competitive spirit of the nation's brightest minds and offers a perfect arena for sports enthusiasts to face off against one another. As said by Serena Williams, let's overpower, overtake and overcome!`,
+    imgUrl: '/assets/mashallogo.png'
   },
   {
     title: 'Udgam',
-    description: 'lorem ipsum',
-    imgUrl: '/assets/udayam.png'
+    description: `Udgam is the Annual Cultural Festival of the department of Electronics Engineering, IIT (BHU) Varanasi. It brings you multifarious events that kindle your creative side and awaken your artistic self. EES introduced Udgam at the start of 2022 to provide a platform for participants to showcase their talents in non-technical fields like fine arts, dance, music, photography, and literature. Udgam is back again, bigger and brighter, and awaiting to witness the vigour its participants would bring to the stage. Get ready to manifest your artistry and let your creative self sparkle!`,
+    imgUrl: '/assets/udgamlogo.png'
   }
 ];
 
