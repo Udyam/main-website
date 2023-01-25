bcfaa63db2214e80001cbd15284d8130590cb324
Dhruv <dhruvchaudharycar1382003@gmail.com>
2023-01-25 21:54:20 +0530

    Events title nd desc added

--- src/Udgam/components/Events/udgamEvents.js
+++ src/Udgam/components/Events/udgamEvents.js
@@ -14,39 +14,46 @@ function UdgamEvents() {
     {
       id: 1,
       genre: 'Photography',
-      subEvents: ['Photog Event 1', 'Photog Event 2'],
+      subEvents: ['Campus in Pixels', 'Life in Kashi'],
+      desc: 'The feelings for this centuries-old city remain the same, whatever changes. In this era of fleeting moments, Udgam gives you the opportunity to discover your inner photographer and capture the beauty of the campus and the city. So, pick up your cameras and show the world the beauty of Kashi.',
       subEventsImg: [bg, bgg]
     },
     {
       id: 2,
       genre: 'Dance',
-      subEvents: ['Dance Event 1']
+      desc: '“Do it big, do it right, and do it with style.” - Fred Astaire If you have the potential to sweep the audience off their feet and dance to the top, then Udgam’s stage is the right place to exhibit your talent. Udgam presents its dance event for you to make the move on the beat.',
+      subEvents: ['Ignite']
     },
     {
       id: 3,
       genre: 'Art',
-      subEvents: ['Art Event 1', 'Art Event 2', 'Art Event 3']
+      desc: 'Do you have a zeal for art and want to compete with the best? If yes, Udgam brings you the opportunity to show your talent at its art event. So, pick up your pencils and paintbrushes, as the time has come to let your imaginations run wild.',
+      subEvents: ['India in 2050', 'Character Designing', 'Face Painting']
     },
     {
       id: 4,
       genre: 'Music',
-      subEvents: ['Music Event 1']
+      desc: '“Where words fail, music speaks.” Udgam is setting the stage for its music event in its new edition. It’s time to showcase the flair of your voice and stun the masses!',
+      subEvents: ['Ridham']
     },
     {
       id: 5,
       genre: 'Literature',
-      subEvents: ['Literature Event 1', 'Literature Event 2']
+      desc: '“Literature is the immortality of speech”.To keep the spirits of cultural events high, Udgam presents literary events like Poetry, Shayari, and more, with an intent to celebrate the author, the poet, and the orator hidden within you.',
+      subEvents: ['Open Mic Stand', 'Creative Writing']
     },
     {
       id: 6,
       genre: 'Treasure Hunt',
-      subEvents: ['Treasure hunt Event 1']
+      desc: 'As exciting as it sounds, gear up to become sleuth-hounds! The campus awaits to grace the exquisite event of Treasure Hunt, an unmissable and amazing opportunity to bring out your inner Sherlock and join the blazing quest of finding the enigmatic trove.',
+      subEvents: ['Treasure Hunt']
     }
   ];
   const [eventTitle, setEventTitle] = useState(events[0].subEvents[0]);
   const [eventtable, setEventtable] = useState(true);
   const [whichEvent, setwhichEvent] = useState('Photography');
   const [subEventImg, setsubEventImg] = useState(events[0].subEventsImg[0]);
+  const [eventDesc, setEventDesc] = useState(events[0].desc);
   // const [eventnav, setEventnav] = useState(false);
   const [clickct, setClickct] = useState(0);
   const eventName = (event) => {
@@ -56,6 +63,7 @@ function UdgamEvents() {
     let obj = events.find((o) => o.genre === element.innerText);
     setEventTitle(obj.subEvents[clickct]);
     setsubEventImg(obj.subEventsImg[clickct]);
+    setEventDesc(obj.desc);
     setEventtable(false);
     console.log(element);
   };
@@ -163,7 +171,7 @@ function UdgamEvents() {
           <div className="event-box">
             <div className="event-title">{eventTitle}</div>
             <div className="event-desc-box">
-              <div className="event-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
+              <div className="event-desc">{eventDesc}</div>
             </div>
             <div className="event-buttons">
               <div className="register">Register</div>
