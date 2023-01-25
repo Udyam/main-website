7e71cdf955aeb90b0058d46fcd65e911f075fe4f
avnesh141 <avnesh.kumar.ece21@itbhu.ac.in>
2023-01-25 23:17:31 +0530

    Update AboutUs.js

--- src/EES/AboutUS/AboutUs.js
+++ src/EES/AboutUS/AboutUs.js
@@ -42,13 +42,13 @@ function AboutUs() {
   const { inViewport } = useInViewport(myRef);
 
   if (inViewport) {
-  document.getElementsByClassName('square1')[0].classList.add('hover1');
-  document.getElementsByClassName('square2')[0].classList.add('hover2');
+    document.getElementsByClassName('square1')[0].classList.add('hover1');
+    document.getElementsByClassName('square2')[0].classList.add('hover2');
   } else {
-  if (document.getElementsByClassName('hover1')[0]) {
-    document.getElementsByClassName('square1')[0].classList.remove('hover1');
-    document.getElementsByClassName('square2')[0].classList.remove('hover2');
-  }
+    if (document.getElementsByClassName('hover1')[0]) {
+      document.getElementsByClassName('square1')[0].classList.remove('hover1');
+      document.getElementsByClassName('square2')[0].classList.remove('hover2');
+    }
   }
 
   return (
@@ -74,7 +74,6 @@ function AboutUs() {
                   The Electronics Engineering Society (EES) is the departmental society of the Department of Electronics Engineering (ECE) of The Indian Institute of Technology, Varanasi (IIT BHU). EES has been organizing its annual techno-management fest, Udyam, for over a decade. It also organises the departmental Sports Event, Mashal, and Cultural Event, Udgam. Through these events, the EES
                   aims to provide practical and holistic interaction and learning to all its event participants.
                 </p>
-                
               </div>
             </div>
             <div className="squares">
@@ -82,7 +81,7 @@ function AboutUs() {
               <div className="square2"></div>
             </div>
           </div>
-          <div className="dots" >
+          <div className="dots">
             <div className="dot-pair">
               <span className="dot"></span>
               <span className="dot"></span>
