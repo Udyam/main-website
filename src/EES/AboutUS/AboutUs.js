0484c92ffe305f9e38780655af7443c7b9ede8e9
avnesh141 <avnesh.kumar.ece21@itbhu.ac.in>
2023-01-25 20:08:21 +0530

    AboutUs

--- src/EES/AboutUS/AboutUs.js
+++ src/EES/AboutUS/AboutUs.js
@@ -1,22 +1,56 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
 import './AboutUs.css';
-import { useInViewport } from 'react-in-viewport';
-import { useRef } from 'react';
+// import { useInViewport } from 'react-in-viewport';
+// import { useState,useEffect } from 'react';
 
 function AboutUs() {
-  const myRef = useRef();
-  const { inViewport } = useInViewport(myRef);
+  // const [y, setY] = useState(0);
 
-  if (inViewport) {
-    document.getElementsByClassName('square1')[0].classList.add('hover1');
-    document.getElementsByClassName('square2')[0].classList.add('hover2');
-  } else {
-    if (document.getElementsByClassName('hover1')[0]) {
-      document.getElementsByClassName('square1')[0].classList.remove('hover1');
-      document.getElementsByClassName('square2')[0].classList.remove('hover2');
-    }
-  }
+  // const handleNavigation = (e) => {
+  //   const window = e.currentTarget;
+  //   setY(window.scrollY);
+  //   if ( y<970 && y>670) {
+  //     console.log(y);
+  //     console.log("scroll UP");
+  //     let item = document.getElementsByClassName('square1')[0];
+  //     console.log(item);
+  //     document.getElementsByClassName('square2')[0].classList.add('hover2');
+  //   }
+  // else {
+  //   if (document.getElementsByClassName('hover1')[0]) {
+  //     document.getElementsByClassName('square1')[0].classList.remove('hover1');
+  //     document.getElementsByClassName('square2')[0].classList.remove('hover2');
+  //   }
+  // }
+  // else if (y < window.scrollY) {
+  //   console.log(y);
+  //   console.log(window.scrollY);
+  //   console.log("scrolling down");
+  // }
+
+  // };
+
+  // useEffect(() => {
+  //   setY(window.scrollY);
+  // document.getElementsByClassName('main-box')[0].addEventListener('scroll', (e) => {
+  //   handleNavigation(e);
+  // })
+  // window.addEventListener("scroll", (e) => handleNavigation(e));
+  // }, []);
+
+  // const myRef = useRef();
+  // const { inViewport } = useInViewport(myRef);
+
+  // if (inViewport) {
+  // document.getElementsByClassName('square1')[0].classList.add('hover1');
+  // document.getElementsByClassName('square2')[0].classList.add('hover2');
+  // } else {
+  // if (document.getElementsByClassName('hover1')[0]) {
+  //   document.getElementsByClassName('square1')[0].classList.remove('hover1');
+  //   document.getElementsByClassName('square2')[0].classList.remove('hover2');
+  // }
+  // }
 
   return (
     <>
@@ -62,7 +96,11 @@ function AboutUs() {
               <span className="dot"></span>
             </div>
             <div className="dot-pair">
-              <span className="dot" ref={myRef}></span>
+              <span className="dot"></span>
+              <span className="dot"></span>
+            </div>
+            <div className="dot-pair">
+              <span className="dot"></span>
               <span className="dot"></span>
             </div>
             <div className="dot-pair">
