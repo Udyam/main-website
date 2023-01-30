b138cea680607c69eab3cfb125181ec3b61fbe74
Nikhil Agarwal <agarwalnikhil576@gmail.com>
2023-01-31 04:30:54 +0530

    mashal leaderboard

--- src/Mashal/components/MashalLeaderboard/MashalLeaderboard.js
+++ src/Mashal/components/MashalLeaderboard/MashalLeaderboard.js
@@ -9,27 +9,27 @@ function MashalLeaderboard() {
         <div className="heading-container">TOP PLAYERS SCORE</div>
         <div className="leaderboard-score-container">
           <div className="first-team teams">
-            <div></div>
+            <div className="positionBadge"></div>
             <div>TEAM NAME</div>
             <div className="score">100</div>
           </div>
           <div className="second-team teams">
-            <div></div>
+            <div className="positionBadge"></div>
             <div>TEAM NAME</div>
             <div className="score">80</div>
           </div>
           <div className="third-team teams">
-            <div></div>
+            <div className="positionBadge"></div>
             <div>TEAM NAME</div>
             <div className="score">70</div>
           </div>
           <div className="fourth-team teams">
-            <div></div>
+            <div className="positionBadge"></div>
             <div>TEAM NAME</div>
             <div className="score">60</div>
           </div>
           <div className="fifth-team teams">
-            <div></div>
+            <div className="positionBadge"></div>
             <div>TEAM NAME</div>
             <div className="score">50</div>
           </div>
