2028e0ef0efc3402c61e5cc1c2444f6125a9dccb
Dhruv <dhruvchaudharycar1382003@gmail.com>
2023-01-30 21:38:16 +0530

    images added

--- src/Udgam/components/Events/udgamEvents.js
+++ src/Udgam/components/Events/udgamEvents.js
@@ -1,9 +1,4 @@
 import './events.css';
-import bg from './photography.png';
-import bg1 from './Ellipse2.png';
-import bg2 from './Ellipse4.png';
-import bgg from './bg2.png';
-// import Nav from '../Nav/Nav';
 
 import { useState, useEffect } from 'react';
 import { MdArrowForwardIos } from 'react-icons/md';
@@ -16,37 +11,42 @@ function UdgamEvents() {
       genre: 'Photography',
       subEvents: ['Campus in Pixels', 'Life in Kashi'],
       desc: 'The feelings for this centuries-old city remain the same, whatever changes. In this era of fleeting moments, Udgam gives you the opportunity to discover your inner photographer and capture the beauty of the campus and the city. So, pick up your cameras and show the world the beauty of Kashi.',
-      subEventsImg: [bg, bgg]
+      subEventsImg: ['campusInPixels', 'lifeInKashi']
     },
     {
       id: 2,
       genre: 'Dance',
-      desc:'“Do it big, do it right, and do it with style.” - Fred Astaire If you have the potential to sweep the audience off their feet and dance to the top, then Udgam’s stage is the right place to exhibit your talent. Udgam presents its dance event for you to make the move on the beat.',
-      subEvents: ['Ignite']
+      desc: '“Do it big, do it right, and do it with style.” - Fred Astaire If you have the potential to sweep the audience off their feet and dance to the top, then Udgam’s stage is the right place to exhibit your talent. Udgam presents its dance event for you to make the move on the beat.',
+      subEvents: ['Ignite'],
+      subEventsImg: ['ignite']
     },
     {
       id: 3,
       genre: 'Art',
-      desc:'Do you have a zeal for art and want to compete with the best? If yes, Udgam brings you the opportunity to show your talent at its art event. So, pick up your pencils and paintbrushes, as the time has come to let your imaginations run wild.',
-      subEvents: ['India in 2050', 'Character Designing', 'Face Painting']
+      desc: 'Do you have a zeal for art and want to compete with the best? If yes, Udgam brings you the opportunity to show your talent at its art event. So, pick up your pencils and paintbrushes, as the time has come to let your imaginations run wild.',
+      subEvents: ['India in 2050', 'Character Designing', 'Face Painting'],
+      subEventsImg: ['indiaIn2050', 'Character Design', 'facePainting']
     },
     {
       id: 4,
       genre: 'Music',
-      desc:'“Where words fail, music speaks.” Udgam is setting the stage for its music event in its new edition. It’s time to showcase the flair of your voice and stun the masses!',
-      subEvents: ['Ridham']
+      desc: '“Where words fail, music speaks.” Udgam is setting the stage for its music event in its new edition. It’s time to showcase the flair of your voice and stun the masses!',
+      subEvents: ['Ridham'],
+      subEventsImg: ['ridham']
     },
     {
       id: 5,
       genre: 'Literature',
-      desc:'“Literature is the immortality of speech”.To keep the spirits of cultural events high, Udgam presents literary events like Poetry, Shayari, and more, with an intent to celebrate the author, the poet, and the orator hidden within you.',
-      subEvents: ['Open Mic Stand', 'Creative Writing']
+      desc: '“Literature is the immortality of speech”.To keep the spirits of cultural events high, Udgam presents literary events like Poetry, Shayari, and more, with an intent to celebrate the author, the poet, and the orator hidden within you.',
+      subEvents: ['Open Mic Stand', 'Creative Writing'],
+      subEventsImg: ['openMic', 'creativeWriting']
     },
     {
       id: 6,
       genre: 'Treasure Hunt',
-      desc:'As exciting as it sounds, gear up to become sleuth-hounds! The campus awaits to grace the exquisite event of Treasure Hunt, an unmissable and amazing opportunity to bring out your inner Sherlock and join the blazing quest of finding the enigmatic trove.',
-      subEvents: ['Treasure Hunt']
+      desc: 'As exciting as it sounds, gear up to become sleuth-hounds! The campus awaits to grace the exquisite event of Treasure Hunt, an unmissable and amazing opportunity to bring out your inner Sherlock and join the blazing quest of finding the enigmatic trove.',
+      subEvents: ['Treasure Hunt'],
+      subEventsImg: ['treasureHunt']
     }
   ];
   const [eventTitle, setEventTitle] = useState(events[0].subEvents[0]);
@@ -54,23 +54,27 @@ function UdgamEvents() {
   const [whichEvent, setwhichEvent] = useState('Photography');
   const [subEventImg, setsubEventImg] = useState(events[0].subEventsImg[0]);
   const [eventDesc, setEventDesc] = useState(events[0].desc);
-  // const [eventnav, setEventnav] = useState(false);
+  const [circleOne, setCircleOne] = useState(events[0].subEventsImg[1]);
+  const [circleTwo, setCircleTwo] = useState(events[0].subEventsImg[1]);
   const [clickct, setClickct] = useState(0);
   const eventName = (event) => {
     var element = event.target;
     setwhichEvent(element.innerText);
     setClickct(0);
     let obj = events.find((o) => o.genre === element.innerText);
-    setEventTitle(obj.subEvents[clickct]);
+    let l = obj.subEvents.length;
     setsubEventImg(obj.subEventsImg[clickct]);
     setEventDesc(obj.desc);
+    setEventTitle(obj.subEvents[clickct]);
     setEventtable(false);
-    console.log(element);
+    setCircleOne(obj.subEventsImg[(clickct - 1 + l) % l]);
+    setCircleTwo(obj.subEventsImg[(clickct + 1) % l]);
+    console.log(obj);
   };
 
   const [width, setWidth] = useState(window.innerWidth);
 
-  function checkNav() {
+  const checkNav = () => {
     if (width > 800) {
       return;
     } else {
@@ -81,7 +85,7 @@ function UdgamEvents() {
       document.querySelector('.udyam-nav').style.display = 'none';
       // }
     }
-  }
+  };
 
   const updateWidth = () => {
     setWidth(window.innerWidth);
@@ -107,6 +111,7 @@ function UdgamEvents() {
 
   const circleOneClicked = () => {
     let obj = events.find((o) => o.genre === whichEvent);
+    let l = obj.subEvents.length;
     if (clickct <= 0) {
       setClickct(obj.subEvents.length - 1);
     } else {
@@ -114,9 +119,12 @@ function UdgamEvents() {
     }
     setEventTitle(obj.subEvents[clickct]);
     setsubEventImg(obj.subEventsImg[clickct]);
+    setCircleOne(obj.subEventsImg[(clickct - 1 + l) % l]);
+    setCircleTwo(obj.subEventsImg[(clickct + 1) % l]);
   };
   const circleTwoClicked = () => {
     let obj = events.find((o) => o.genre === whichEvent);
+    let l = obj.subEvents.length;
     if (clickct >= obj.subEvents.length - 1) {
       setClickct(0);
     } else {
@@ -124,9 +132,11 @@ function UdgamEvents() {
     }
     setEventTitle(obj.subEvents[clickct]);
     setsubEventImg(obj.subEventsImg[clickct]);
+    setCircleTwo(obj.subEventsImg[(clickct + 1) % l]);
+    setCircleOne(obj.subEventsImg[(clickct - 1 + l) % l]);
     console.log(clickct);
   };
-
+  let obj = events.find((o) => o.genre === whichEvent);
   return (
     <div>
       {(eventtable || width > 800) && (
@@ -181,9 +191,9 @@ function UdgamEvents() {
         </div>
       </div>
       <div className="event-img-pc">
-        <img className="circle-one" src={bg1} onClick={circleOneClicked} />
-        <img className="circle-two" onClick={circleTwoClicked} src={bg2} />
-        <img className="main-img" src={subEventImg} />
+        {obj.subEvents.length > 1 && <img className="circle-one" src={'/assets/udgam-events/images/' + circleOne + '.jpg'} onClick={circleOneClicked} />}
+        {obj.subEvents.length > 1 && <img className="circle-two" onClick={circleTwoClicked} src={'/assets/udgam-events/images/' + circleTwo + '.jpg'} />}
+        <img className="main-img" src={'/assets/udgam-events/images/' + subEventImg + '.jpg'} />
         {/* <div className="circle-one" onClick={circleOneClicked}>
           <img src={bg1} />
         </div>
